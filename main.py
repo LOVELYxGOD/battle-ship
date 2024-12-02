8ce1670eca7dc5d37fbc406a5e8f19ade5ab530e
LOVELY <kotagindeman@gmail.com>
2024-12-02 17:57:02 +0300

    feat: add function  display

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ class Field:
             self.grid.append([None] * size)
         self.ships_alive = ships
 
+    def display(self, show_ships=False):
+        letters = 'ABCDEFGHIJ'
+        space = '    '
 
+        for letter in letters:
+            space += letter + ' '
+        print(space)
+        for i, row in enumerate(self.grid):
+            display_row = ""
+            for cell in row:
+                if cell is None or (cell is not None and not show_ships):
+                    display_row += "O "
+                else:
+                    display_row += "■ "
+            if i + 1 != 10:  # вывод ноликов и квадратиков
+                print(i + 1, " ", display_row)
+            else:
+                print(i + 1, "", display_row)
